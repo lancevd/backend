4bb429effa4b6bb3b18430f31f6c5b4c956deb8e
lancevd <olamosob@gmail.com>
2025-03-24 13:05:14 +0100

    user.model

---
+++ models/user.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    tokens: {
+      type: Number,
+      default: 500,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
